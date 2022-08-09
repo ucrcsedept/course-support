d7cacd664858d6de8f0481e84dca945ac5a86f6a
Hephaestus <thereadingmachine@gmail.com>
2022-08-09 13:26:50 -0700

    Files needed for complete cs130 transition added

---
+++ docker/cs130/glfw-3/tests/CMakeFiles/clipboard.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/usr/csshare/src/glfw-master/deps/getopt.c" "/usr/csshare/pkgs/glfw-3/tests/CMakeFiles/clipboard.dir/__/deps/getopt.c.o"
+  "/usr/csshare/src/glfw-master/tests/clipboard.c" "/usr/csshare/pkgs/glfw-3/tests/CMakeFiles/clipboard.dir/clipboard.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/usr/csshare/pkgs/glfw-3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/csshare/src/glfw-master/include"
+  "/usr/csshare/src/glfw-master/deps"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
