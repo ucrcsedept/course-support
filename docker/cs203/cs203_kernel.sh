f2e80a94aa7b0353bd997428a28dc367349e5b4d
Victor Hill <vhill@ucr.edu>
2022-10-13 18:17:02 -0700

    SINGULARITYENV -> APPTAINERENV

--- docker/cs203/cs203_kernel.sh
+++ docker/cs203/cs203_kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export PATH=/opt/conda/bin:$PATH
-export SINGULARITYENV_PS1='[cs203 container \u@\h \W]\$ '
-export SINGULARITYENV_MKL_NUM_THREADS=1
-export SINGULARITYENV_NUMEXPR_NUM_THREADS=1
-export SINGULARITYENV_OMP_NUM_THREADS=1
+export APPTAINERENV_PS1='[cs203 container \u@\h \W]\$ '
+export APPTAINERENV_MKL_NUM_THREADS=1
+export APPTAINERENV_NUMEXPR_NUM_THREADS=1
+export APPTAINERENV_OMP_NUM_THREADS=1
 
 exec /usr/bin/singularity exec /usr/local/containers/cs203.sif /opt/conda/bin/python3 -m ipykernel_launcher $@
 
