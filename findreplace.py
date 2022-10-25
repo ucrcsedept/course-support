530e396ec7bd42b690a65aa68ee972678f78c1a6
jecndlria <jecndlria@gmail.com>
2022-10-25 19:28:10 +0000

    stashing so i can test regex

--- findreplace.py
+++ findreplace.py
@@ -13,14 +13,17 @@ replaceString = '<a target="_blank" rel="noopener noreferrer" href="\2">\1</a>'
 for i in range(4):
     for file in glob.glob(currentDepth):
         mdFiles.append(file)
-        f = open(file, "r")
-        fileContents = f.read()
-        listOfMatches = re.findall("\[(.*?)\]\((.*?)\)", fileContents)
-        #re.sub(matchingString, replaceString, fileContents)
-        print(listOfMatches)
-        f.close()
         # print(file)
     currentDepth = currentDepth[:2] + "*/" + currentDepth[2:]
     # print(currentDepth)
 
-# print(mdFiles)   
+for file in mdFiles:
+        f = open(file, "r")
+        fileContents = f.read()
+        listOfMatches = re.findall("\[(.*?)\]\((.*?)\)", fileContents)
+        fileContentsNew = re.sub(matchingString, replaceString, fileContents)
+        f.write(fileContentsNew)
+        print(listOfMatches)
+        f.close()
+
+# print(mdFiles) 
