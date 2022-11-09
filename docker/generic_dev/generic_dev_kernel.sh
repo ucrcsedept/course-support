5e211675c4e7f6d7d1b0b483dc105b85b288d3d3
jecndlria <jecndlria@gmail.com>
2022-11-09 02:28:50 +0000

    added boilerplate for kernel/bash script files

---
+++ docker/generic_dev/generic_dev_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PATH=/opt/conda/bin:$PATH
+export APPTAINERENV_PS1='[generic_dev container \u@\h \W]\$ '
+export APPTAINERENV_MKL_NUM_THREADS=1
+export APPTAINERENV_NUMEXPR_NUM_THREADS=1
+export APPTAINERENV_OMP_NUM_THREADS=1
+
+exec /usr/bin/singularity exec /usr/local/containers/cs105.sif /opt/conda/bin/python3 -m ipykernel_launcher $@
+
