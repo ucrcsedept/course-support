1aa64ef9f0b26af02acdfe0773c05f9baf5c35cd
kennythai <thaikenny2003@gmail.com>
2023-09-01 20:07:49 -0700

    Created Podman base containers for remaining undergraduate courses

---
+++ containers/cs173/cs173_kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH=/opt/conda/bin:$PATH
+export APPTAINERENV_PS1='[cs173 container \u@\h \W]\$ '
+export APPTAINER_BIND=/usr/local
+export APPTAINERENV_MKL_NUM_THREADS=1
+export APPTAINERENV_NUMEXPR_NUM_THREADS=1
+export APPTAINERENV_OMP_NUM_THREADS=1
+
+exec /usr/bin/singularity exec /usr/local/containers/cs173.sif /opt/conda/bin/python3 -m ipykernel_launcher $@
+
